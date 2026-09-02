b160c8042211cc8b91758a316f80932752d14e3c
02adibaramzan <02adibaramzan@gmail.com>
2026-09-02 18:01:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 # Page Settings (Website ka Title aur Design)
 st.set_page_config(
     page_title=Universal Video Downloader,
-    page_icon=📥,
+    page_icon="📥",
     layout=centered
 )
 
